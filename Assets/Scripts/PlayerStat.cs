57346aa7ac278501e84fbf1253a66a14014dcddb
Lupusanghrenn <brisingr34130@gmail.com>
2020-02-14 11:23:23 +0100

    push player stat et ignore de .vscode

---
+++ Assets/Scripts/PlayerStat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New PlayerStat",menuName="PlayerStat")]
+public class PlayerStat : ScriptableObject
+{
+    public float Health;
+    public float Speed;
+    public float JumpHeight;
+    public float Attack;
+    public float RateOfFire;
+    public float Luck;
+    public float Lifesteal;
+}
